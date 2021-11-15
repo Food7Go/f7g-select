a15d43f93df0a632a98156c5dfcebe054b68149b
daifAB <abd.daif96@gmail.com>
2021-11-15 16:09:36 +0100

    f7g-select done

--- projects/f7g-select/src/lib/f7g-select.component.ts
+++ projects/f7g-select/src/lib/f7g-select.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
-  selector: 'lib-f7g-select',
+  selector: 'f7g-select',
   templateUrl: './f7g-select.component.html',
-  styleUrls: ['./f7g-select.component.css']
+  styleUrls: ['./f7g-select.component.css'],
 })
 export class F7gSelectComponent implements OnInit {
+  @Input() data: any = [];
+  @Input() multiple: boolean = false;
+  @Input() searcheable: boolean = false;
+  @Input() label: string = '';
+  @Input() placeholder: string = '';
 
-  constructor() { }
+  @Output() output = new EventEmitter<any>();
+
+  selectedData: any;
+  constructor() {}
 
   ngOnInit(): void {
+    this.selectedData = this.data.sort(
+      (a: any, b: any) => (a.name > b.name && 1) || -1
+    );
   }
 
+  onKey(event: any) {
+    this.selectedData = this.search(event.value);
+  }
+
+  search(value: string) {
+    let filter = value.toLowerCase();
+    return this.data.filter((option: any) =>
+      option.name.toLowerCase().startsWith(filter)
+    );
+  }
+
+  emitValue(event: any) {
+    if (typeof event.value === 'string') {
+      this.output.emit([event.value]);
+    } else {
+      this.output.emit(event.value);
+    }
+  }
 }
--- projects/f7g-select/src/lib/f7g-select.module.ts
+++ projects/f7g-select/src/lib/f7g-select.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { F7gSelectComponent } from './f7g-select.component';
-
-
+// mat component
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-  declarations: [
-    F7gSelectComponent
-  ],
-  imports: [
-  ],
-  exports: [
-    F7gSelectComponent
-  ]
+  declarations: [F7gSelectComponent],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, CommonModule],
+  exports: [F7gSelectComponent],
 })
-export class F7gSelectModule { }
+export class F7gSelectModule {}
